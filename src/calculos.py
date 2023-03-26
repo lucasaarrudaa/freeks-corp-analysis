55dae16e01e96a7c422f0ac3ba79a97dd8e14dd8
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-03-26 17:53:01 -0300

    update calculos

--- src/calculos.py
+++ src/calculos.py
@@ -9,6 +9,7 @@ class Campanha:
         ROI = FLOAT/INT ex: (12.5) ou (50)
         vendas = INT    ex: (1000)
     '''
+
     def __init__(self, campanha, cpc, impr, ctr, tx, roi, vendas):
 
         self.nome = campanha.upper()
@@ -26,18 +27,21 @@ class Campanha:
         self.receita_mensal = self.receita_diaria * 30
         self._rpa = self.receita_diaria / self.vendas_dia
         self.lucro_mensal = self.lucro_diario * 30
-        
+
     def crise(self):
-        
+
         self.novo_roi = 1
         self.novo_rpa = 10
         self.nova_receita_diaria = (self.novo_rpa * self.vendas_dia)
         self.novo_custo_diario = self.nova_receita_diaria / 2
-        self.novo_cpc = self.novo_custo_diario / (self.impressoes_diarias * self.ctr)
-        self.novo_lucro_mensal = (self.nova_receita_diaria - self.novo_custo_diario) * 30
+        self.novo_cpc = self.novo_custo_diario / \
+            (self.impressoes_diarias * self.ctr)
+        self.novo_lucro_mensal = (
+            self.nova_receita_diaria - self.novo_custo_diario) * 30
         return print(f'\n\
-                        \nNovo CPC da campanha *{self.nome}* deve ser {self.novo_cpc:,.2f}\
-                        \nO novo lucro mensal deverá ser R$ {self.novo_lucro_mensal:,.2f}')    
+                        \nNovo CPC da campanha *{self.nome}* deve ser de {self.novo_cpc:,.2f}\
+                        \nO novo lucro mensal deverá ser de R$ {self.novo_lucro_mensal:,.2f}')
+
     def __str__(self):
 
         self.dados = f'\n\
@@ -50,4 +54,3 @@ class Campanha:
                     \nReceita Diária: R$ {self.receita_diaria:,.2f}\
                     \nReceita Mensal: R$ {self.receita_mensal:,.2f};'
         return self.dados
-    
