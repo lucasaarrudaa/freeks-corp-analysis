534f89063459a72267b4931332361c6e3574097f
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-03-26 17:18:15 -0300

    função para mostrar dados previstos

---
+++ src/dados_previsao.py
@@ -0,0 +1,11 @@
+from src.campanhas import a,b,c,d
+
+def mostrar():
+    '''
+    Mostra os dados para a crise que está prevista para o segundo mês
+    '''
+    a.crise()
+    b.crise()
+    c.crise()
+    d.crise()
+    
