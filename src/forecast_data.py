d54a453c24c246881c6d8652738955420241afbb
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-13 11:27:09 -0300

    show forecast data

---
+++ src/forecast_data.py
@@ -0,0 +1,11 @@
+from src.campaigns import a, b, c, d
+
+
+def show():
+    '''
+    Shows the data for the crisis that is predicted for the second month.
+    '''
+    a.crisis()
+    b.crisis()
+    c.crisis()
+    d.crisis()
