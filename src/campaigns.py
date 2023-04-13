65d6ba793b3cd8da54b8c3d28fe8e7e453747545
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-13 11:26:45 -0300

    campaign data

---
+++ src/campaigns.py
@@ -0,0 +1,33 @@
+from src.calculations import Campaign
+
+a = Campaign(campaign='a',
+             cpc=0.05,
+             impressions=12484395,
+             ctr=0.89,
+             cvr=0.90,
+             roi=100,
+             sales=1000)
+
+b = Campaign(campaign='b',
+             cpc=0.1,
+             impressions=2884615,
+             ctr=1.20,
+             cvr=2.60,
+             roi=150,
+             sales=900)
+
+c = Campaign(campaign='c',
+             cpc=0.15,
+             impressions=6920415,
+             ctr=1.02,
+             cvr=1.70,
+             roi=80,
+             sales=1200)
+
+d = Campaign(campaign='d',
+             cpc=0.1,
+             impressions=3885004,
+             ctr=1.98,
+             cvr=1.30,
+             roi=70,
+             sales=1000)
